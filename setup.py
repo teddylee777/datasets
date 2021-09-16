9d6d40d6c7e4d12840763a7f6e47a28148cac69b
Teddy Lee <teddylee777@gmail.com>
2021-09-16 23:49:24 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teddynote',
+    version='0.0.1',
+    description='datasets and tutorial package made and maintained by TeddyNote',
+    author='teddylee777',
+    author_email='teddylee777@gmail.com',
+    url='https://github.com/teddylee777/datasets',
+    install_requires=['tqdm'],
+    packages=find_packages(exclude=[]),
+    keywords=['teddynote', 'teddylee777', 'python datasets', 'python tutorial', 'machine learning', 'deep learning'],
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
---
+++ teddynote/dataset.py
@@ -0,0 +1,4 @@
+
+
+# 데이터셋 JSON 파일 경로
+DATASET_URL = 'https://raw.githubusercontent.com/braincrew/cds/main/mySUNI/data/dataset.json'
